235f0143c999119b2d5bd846eec4c0ec8d757789
Lordir <09mmnn@gmail.com>
2022-08-02 12:36:15 +0700

    auth finish

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
+import pickle
 from selenium.webdriver.common.by import By
 
 # options
@@ -18,25 +19,18 @@ def get_source_html(url):
     driver.maximize_window()
 
     try:
-        # driver.get(url=url)
         driver.get(url=url)
         time.sleep(5)
-        # login_button = driver.find_element(By.CLASS_NAME,
-        #                                    "base-ui-button-v2__text").click()
-        login_button = driver.find_element(By.CLASS_NAME,
-                                           "presearch__suggests")
-        login_button.clear()
-        login_button.click()
-        login_button.send_keys("rtx")
-        login_button.click()
-        # print(driver.page_source)
-        # email_input = driver.find_element(By.ID, "ir-wp7opg")
-        # email_input.clear()
-        # email_input.send_keys("email")
-        # password_input = driver.find_element(By.ID, "ir-o9898")
-        # email_input.clear()
-        # email_input.send_keys("password")
+        for cookie in pickle.load(open("cookies", "rb")):
+            driver.add_cookie(cookie)
         time.sleep(5)
+        driver.refresh()
+        # pickle.dump(driver.get_cookies(), open("cookies", "wb"))
+
+        # print(driver.page_source)
+        # password_input = driver.find_element(By.ID, "ir-o9898")
+
+        time.sleep(10)
 
     except Exception as _ex:
         print(_ex)
@@ -49,5 +43,6 @@ def main():
     get_source_html(url="https://www.dns-shop.ru/profile/wishlist/")
 
 
+# "https://www.dns-shop.ru/profile/wishlist/"
 if __name__ == "__main__":
     main()
